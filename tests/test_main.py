fac189eaba5a1e3807bd2b85d74a171049f70eb2
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-03-23 16:38:48 +0100

    update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python3
 
-# SCRIPT NAME - TESTS
-# 2023 (c) Micha Johannes Birklbauer
-# https://github.com/michabirklbauer/
-# micha.birklbauer@gmail.com
-
 
 def test1():
-    import pandas as pd
+    from main import count_csms
 
-    df = pd.read_csv("data/example_data.csv")
-
-    from main import my_product
-
-    for i, row in df.iterrows():
-        assert my_product(int(row["x"]), int(row["y"])) == int(row["x"]) * int(row["y"])
-
-
-def test2():
-    from main import main
-
-    assert main(["-f1", "20"]) == 40
-    assert main(["-f1", "2", "-f2", "3"]) == 6
+    nr_csms: int = count_csms("data/csms.txt")
+    assert nr_csms == 826
